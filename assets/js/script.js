72377145f889b27cdbbfebcf1ae2a7ba64e58409
adampriley1 <adampriley1@hotmail.com>
2023-12-16 09:30:06 +0000

    fetched current weather from api

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,8 @@ $(".search-button").on("click", function (event) {
   //fetching from api
 
   var citySearch = $("#search-input").val();
-  const queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + citySearch + "&appid=5182d0376e53c61cba7fadacb43513d8&";
+  const queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + citySearch + "&appid=5182d0376e53c61cba7fadacb43513d8&units=metric";
+
 
   fetch(queryURL)
     .then(function (response) {
@@ -15,10 +16,28 @@ $(".search-button").on("click", function (event) {
     .then(function (data) {
       console.log(data);
 
-      // const cityData  = data.data.images.original.url;
+      //retriece current weather from API
+      
+        var currentDate = data.list[0].dt_txt;
+        var currentIcon = data.list[0].weather[0].icon;
+        var currentTemp = data.list[0].main.temp;
+        var currentWind = data.list[0].wind.speed;
+        var currentHumidity = data.list[0].main.humidity;
+
+
+
+
+        // const today = $("#today");
+
+
+
+
+       
+    //    today.text(currentWeather);
+        
     });
 
-    // Get the value from the search input using jQuery
+   // Get the value from the search input using jQuery
     
   });
 
